5fb88e6440b5c726f4bcd1cdc834dbaba1c012c2
Rupesh3000 <rupeshpatil11121@gmail.com>
2025-02-25 16:25:21 +0000

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen bg-black'>
+      <h1 className='text-center text-2xl text-white md:text-red-700 md:text-3xl'>Now this should be wrok</h1>
+    </div>
+  )
+}
+
+export default App
