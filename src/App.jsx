21301a48f5307e9cf5ed69caffe20d28e8d51ca3
Rupesh3000 <rupeshpatil11121@gmail.com>
2025-03-11 17:10:47 +0000

    add contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,21 @@ import Banner from "./assets/banner-bg.png";
 import AnimatedBackground from "./Components/AnimatedBackground";
 import About from "./Pages/About";
 import Portfolio from "./Pages/Portfolio";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <>
       <div className="wrapper w-full bg-[#090925]">
-        {/* <img
-          src={Banner}
-          alt="Home Banner"
-          className="w-full object-cover min-h-screen  absolute"
-          /> */}
         {/* <AnimatedBackground /> */}
-        <main className=" w-full ">
-          <div className="container max-w-[80rem] text-white mx-auto">
-            {/* <NavBar />
+        <main className=" w-full">
+          <div className="container max-w-[80rem] text-white mx-auto ">
+            <NavBar />
             <HeroSection />
             <About />
-            <Portfolio /> */}
+            <Portfolio />
+            <Contact />
           </div>
-
-          {/* <div className=" bg-black min-h-screen rounded-tl-[50px] rounded-tr-[50px]">
-            <div className="container max-w-[80rem] text-white mx-auto">
-              <About />
-            </div>
-           
-          </div> */}
         </main>
       </div>
     </>
